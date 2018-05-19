c93e1b88cba56ccb264f53a238713b2f78d3eec8
davidmarkclements <huperekchuno@googlemail.com>
2018-05-20 00:56:17 +0200

    benchmark multiple object instances

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,43 +1,44 @@
 'use strict'
 const bench = require('fastbench')
-const obj = require('./fixture.json')
 const deepCopy = require('deep-copy')
 const cloneDeep = require('lodash.clonedeep')
 const fastCopy = require('fast-copy').default
 const clone = require('..')
 const cloneDefaults = clone()
 const cloneProto = clone({proto: true})
-
+const { readFileSync } = require('fs')
+const { join } = require('path')
 const max = 1000
+const objs = [...Array(max)].map(() => JSON.parse(readFileSync(join(__dirname, 'fixture.json'))))
 
 var run = bench([
   function benchDeepCopy (cb) {
     for (var i = 0; i < max; i++) {
-      deepCopy(obj)
+      deepCopy(objs[i])
     }
     setImmediate(cb)
   },
   function benchLodashCloneDeep (cb) {
     for (var i = 0; i < max; i++) {
-      cloneDeep(obj)
+      cloneDeep(objs[i])
     }
     setImmediate(cb)
   },
   function benchFastCopy (cb) {
     for (var i = 0; i < max; i++) {
-      fastCopy(obj)
+      fastCopy(objs[i])
     }
     setImmediate(cb)
   },
   function benchRfdc (cb) {
     for (var i = 0; i < max; i++) {
-      cloneDefaults(obj)
+      cloneDefaults(objs[i])
     }
     setImmediate(cb)
   },
   function benchRfdcProto (cb) {
     for (var i = 0; i < max; i++) {
-      cloneProto(obj)
+      cloneProto(objs[i])
     }
     setImmediate(cb)
   }
