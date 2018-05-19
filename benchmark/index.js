3e5029dfe98476936e402f624ed1779e0f597f37
davidmarkclements <huperekchuno@googlemail.com>
2018-05-19 22:11:16 +0200

    update benchmarks

--- benchmark/index.js
+++ benchmark/index.js
@@ -3,6 +3,7 @@ const bench = require('fastbench')
 const obj = require('./fixture.json')
 const deepCopy = require('deep-copy')
 const cloneDeep = require('lodash.clonedeep')
+const fastCopy = require('fast-copy').default
 const clone = require('..')
 const cloneDefaults = clone()
 const cloneProto = clone({proto: true})
@@ -19,6 +20,12 @@ var run = bench([
   function benchLodashCloneDeep (cb) {
     for (var i = 0; i < max; i++) {
       cloneDeep(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchFastCopy (cb) {
+    for (var i = 0; i < max; i++) {
+      fastCopy(obj)
     }
     setImmediate(cb)
   },
