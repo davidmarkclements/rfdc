d4f7fc4ed8f6a2146da624ab4f64b1ce79f0c750
David Mark Clements <huperekchuno@googlemail.com>
2019-05-12 19:56:24 +0200

    shallow bench

---
+++ benchmark/shallow.js
@@ -0,0 +1,71 @@
+'use strict'
+const bench = require('fastbench')
+const deepCopy = require('deep-copy')
+const cloneDeep = require('lodash.clonedeep')
+const fastCopy = require('fast-copy').default
+const obj = {a: 'a', b: 'b', c: 'c'}
+const clone = require('..')
+const cloneDefaults = clone()
+const cloneProto = clone({proto: true})
+const cloneCircles = clone({circles: true})
+const cloneCirclesProto = clone({circles: true, proto: true})
+const max = 1000
+
+var run = bench([
+  function benchDeepCopy (cb) {
+    for (var i = 0; i < max; i++) {
+      deepCopy(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchLodashCloneDeep (cb) {
+    for (var i = 0; i < max; i++) {
+      cloneDeep(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchFastCopy (cb) {
+    for (var i = 0; i < max; i++) {
+      fastCopy(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchObjectAssign (cb) {
+    for (var i = 0; i < max; i++) {
+      var x = Object.assign({}, obj)
+    }
+    setImmediate(cb)
+  },
+  function benchObjectSpread (cb) {
+    for (var i = 0; i < max; i++) {
+      var x = {...obj}
+    }
+    setImmediate(cb)
+  },
+  function benchRfdc (cb) {
+    for (var i = 0; i < max; i++) {
+      cloneDefaults(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchRfdcProto (cb) {
+    for (var i = 0; i < max; i++) {
+      cloneProto(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchRfdcCircles (cb) {
+    for (var i = 0; i < max; i++) {
+      cloneCircles(obj)
+    }
+    setImmediate(cb)
+  },
+  function benchRfdcCirclesProto (cb) {
+    for (var i = 0; i < max; i++) {
+      cloneCirclesProto(obj)
+    }
+    setImmediate(cb)
+  }
+], 100)
+
+run(run)
