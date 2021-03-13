4b284f2b8ef1346e0aa1e45071ff69381c97e6ea
JK <u.sslooky@gmail.com>
2021-03-13 22:42:07 +0000

    Fit to standard style.
    Signed-off-by: JK <u.sslooky@gmail.com>

--- test/index.js
+++ test/index.js
@@ -267,22 +267,22 @@ function types (clone, label) {
   })
   test(`${label} - maps`, async ({ same, isNot }) => {
     const map = new Map([['a', 1]])
-    same(Array.from(clone(map)), [['a', 1]], "same value")
-    isNot(clone(map), map, "different object")
+    same(Array.from(clone(map)), [['a', 1]], 'same value')
+    isNot(clone(map), map, 'different object')
   })
   test(`${label} - sets`, async ({ same, isNot }) => {
     const set = new Set([1])
     same(Array.from(clone(set)), [1])
-    isNot(clone(set), set, "different object")
+    isNot(clone(set), set, 'different object')
   })
   test(`${label} - nested maps`, async ({ same, isNot }) => {
     const data = { m: new Map([['a', 1]]) }
-    same(Array.from(clone(data).m), [['a', 1]], "same value")
-    isNot(clone(data).m, data.m, "different object")
+    same(Array.from(clone(data).m), [['a', 1]], 'same value')
+    isNot(clone(data).m, data.m, 'different object')
   })
   test(`${label} - nested sets`, async ({ same, isNot }) => {
     const data = { s: new Set([1]) }
-    same(Array.from(clone(data).s), [1], "same value")
-    isNot(clone(data).s, data.s, "different object")
+    same(Array.from(clone(data).s), [1], 'same value')
+    isNot(clone(data).s, data.s, 'different object')
   })
 }
